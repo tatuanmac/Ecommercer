da36dfbf38a2648cad5c0c0413a232522f24fe88
Tuấn Tạ <tatuanmac@gmail.com>
2020-06-23 23:49:07 +0700

    [Create] Exampage

--- Ecommercer/Source/Edutalk/ExamListentViewModel.cs
+++ Ecommercer/Source/Edutalk/ExamListentViewModel.cs
@@ -1,10 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
+using Ecommercer.Source.Common.Bases;
+using Prism.Navigation;
+using Xamarin.Forms;
+
 namespace Ecommercer.Source.Edutalk
 {
-    public class ExamListentViewModel
+    public class ExamListentViewModel : ViewModelBase
     {
-        public ExamListentViewModel()
+        public bool IsPlaying { get; set; } = true;
+
+        public ExamListentViewModel(INavigationService navigationService) : base(navigationService)
         {
+        }
+
+        Command PlayCmd;
+
+        public Command PlayCommnad => PlayCmd = PlayCmd ?? new Command(StatusListening);
+
+        private void StatusListening()
+        {
+            IsPlaying = !IsPlaying;
         }
     }
 }
---
+++ Ecommercer/Source/Edutalk/ExampleListentModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Ecommercer.Source.Common.Bases;
+
+namespace Ecommercer.Source.Edutalk
+{
+    public enum ListeningEnum
+    {
+        Play, Pause, Stop
+    }
+
+    public class ExampleListentButton: ModelBase
+    {
+        public string ButtonImageSource { get; set; }
+        public ListeningEnum Type { get; set; }
+        public bool IsSelected { get; set; }
+    }
+}
